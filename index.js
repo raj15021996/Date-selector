f32f94643645fe3ce65d4b21c6b5c3d62ac8ac1a
raj15021996 <rajlyf18@gmail.com>
2022-11-21 16:37:43 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const date = new Date();
+
+const renderCalendar = (current) => {
+  date.setDate(1);
+  const monthDays = document.querySelector(".days");
+  const lastDay = new Date(
+    date.getYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+  const prevLastDay = new Date(
+    date.getYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+  const firstDayIndex = date.getDay();
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+  const nextDays = 7 - lastDayIndex - 1;
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+
+    // if (
+    //   i === new Date().getDate() &&
+    //   date.getMonth() === new Date().getMonth()
+    // ) {
+    //   days += `<div id="today" value=${i}>${i}</div>`;
+    // }
+   if(i === Number(current) )
+    {
+      days += `<div id="current" value=${i}>${i}</div>`; 
+    }
+    else{
+      days += `<div class='current1' value=${i}>${i}</div>`;
+    }
+        
+  //  const n=document.getElementById('current1')
+  //   if(i=== Number(current)){
+  //      n.style.backgroundColor='white';
+  //   }
+  }
+   
+
+  console.log(no)
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+renderCalendar();
+
+let setMonths = document.querySelector("#select-month");
+setMonths.addEventListener("click", () => {
+  if (setMonths.value !== '')
+    date.setMonth(setMonths.value);
+  renderCalendar("");
+});
+
+let setYears = document.querySelector("#select-year");
+setYears.addEventListener("change", () => {
+  if (setYears.value !== '')
+    date.setYear(setYears.value);
+    renderCalendar("");
+});
+
+let dateValue=document.querySelector("#date_input");
+let setDates=document.querySelector("#btn");
+let no=[];
+
+setDates.addEventListener("click", () => {
+ let x = dateValue.value;
+  if(x !=='' && !isNaN(x)){
+    date.setDate(x);
+  }
+  renderCalendar(x);
+ dateValue.value="";
+}); 
